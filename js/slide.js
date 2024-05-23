a428b714d4cc5a5aa82aea11eee0a24b7518644e
bomba-cat <bledion.morina@hotmail.com>
2024-05-23 20:06:13 +0200

    hides button now

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,11 @@
+document.getElementById('buttonup').style.display = 'none';
 document.getElementById('buttondown').addEventListener('click', function() {
     document.getElementById('lastframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttondown').style.display = 'none';
+    document.getElementById('buttonup').style.display = 'block';
 });
 document.getElementById('buttonup').addEventListener('click', function() {
     document.getElementById('firstframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttonup').style.display = 'none';
+    document.getElementById('buttondown').style.display = 'block';
 });
