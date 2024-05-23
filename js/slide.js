b078184d84afffd3d73bb489a5e9d74e70671127
bomba-cat <bledion.morina@hotmail.com>
2024-05-23 22:22:08 +0200

    added third page, more animations

--- js/slide.js
+++ js/slide.js
@@ -1,16 +1,36 @@
 window.onload = function (){
     document.getElementById('firstframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttonup1').style.display = 'none';
+    document.getElementById('buttonup2').style.display = 'none';
+    document.getElementById('buttondown2').style.display = 'none';
+    document.getElementById('buttondown1').style.display = 'block';
 }
 
-document.getElementById('buttonup').style.display = 'none';
-
-document.getElementById('buttondown').addEventListener('click', function() {
-    document.getElementById('lastframe').scrollIntoView({ behavior: 'smooth' });
-    document.getElementById('buttondown').style.display = 'none';
-    document.getElementById('buttonup').style.display = 'block';
+document.getElementById('buttondown1').addEventListener('click', function() {
+    document.getElementById('secondframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttondown1').style.display = 'none';
+    document.getElementById('buttonup2').style.display = 'none';
+    document.getElementById('buttondown2').style.display = 'block';
+    document.getElementById('buttonup1').style.display = 'block';
 });
-document.getElementById('buttonup').addEventListener('click', function() {
+document.getElementById('buttondown2').addEventListener('click', function() {
+    document.getElementById('lastframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttondown1').style.display = 'none';
+    document.getElementById('buttondown2').style.display = 'none';
+    document.getElementById('buttonup1').style.display = 'none';
+    document.getElementById('buttonup2').style.display = 'block';
+});
+document.getElementById('buttonup1').addEventListener('click', function() {
     document.getElementById('firstframe').scrollIntoView({ behavior: 'smooth' });
-    document.getElementById('buttonup').style.display = 'none';
-    document.getElementById('buttondown').style.display = 'block';
+    document.getElementById('buttonup1').style.display = 'none';
+    document.getElementById('buttonup2').style.display = 'none';
+    document.getElementById('buttondown2').style.display = 'none';
+    document.getElementById('buttondown1').style.display = 'block';
+});
+document.getElementById('buttonup2').addEventListener('click', function() {
+    document.getElementById('secondframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttondown1').style.display = 'none';
+    document.getElementById('buttonup2').style.display = 'none';
+    document.getElementById('buttondown2').style.display = 'block';
+    document.getElementById('buttonup1').style.display = 'block';
 });
