c85e7680e38ddd4eed5d2e9099c8601f78a5c515
bomba-cat <bledion.morina@hotmail.com>
2024-05-24 12:00:25 +0200

    added projects

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,13 @@ window.onload = function (){
     document.getElementById('buttondown2').style.display = 'none';
     document.getElementById('buttondown1').style.display = 'block';
 }
+window.addEventListener('resize', function(){
+    document.getElementById('firstframe').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('buttonup1').style.display = 'none';
+    document.getElementById('buttonup2').style.display = 'none';
+    document.getElementById('buttondown2').style.display = 'none';
+    document.getElementById('buttondown1').style.display = 'block';
+});
 
 document.getElementById('buttondown1').addEventListener('click', function() {
     document.getElementById('secondframe').scrollIntoView({ behavior: 'smooth' });
