c7b631d3016e3b4967f731c60d30a36f69304dfa
bomba-cat <bledion.morina@hotmail.com>
2024-12-23 00:28:37 +0100

    small update

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var projectsbutton = document.getElementById("projectsbutton")
-var aboutbutton = document.getElementById("aboutbutton")
-var socialsbutton = document.getElementById("socialsbutton")
+var projectsbutton = document.getElementById("projectsbutton");
+var aboutbutton = document.getElementById("aboutbutton");
+var socialsbutton = document.getElementById("socialsbutton");
 
-var about = document.getElementById("about")
+var about = document.getElementById("about");
 var projects = document.getElementById("projects")
-var socials = document.getElementById("socials")
+var socials = document.getElementById("socials");
 
-projectsbutton.addEventListener("click", showProjects)
-aboutbutton.addEventListener("click", showAbout)
-socialsbutton.addEventListener("click", showSocials)
+projectsbutton.addEventListener("click", showProjects, true)
+aboutbutton.addEventListener("click", showAbout, true);
+socialsbutton.addEventListener("click", showSocials, true)
 
 function showProjects() {
-  projects.style.display = 'inline'
-  about.style.display = 'none'
-  socials.style.display = 'none'
+  projects.style.display = 'flex';
+  about.style.display = 'none';
+  socials.style.display = 'none';
 }
 
 function showAbout() {
-  projects.style.display = 'none'
-  about.style.display = 'inline'
-  socials.style.display = 'none'
+  projects.style.display = 'none';
+  about.style.display = 'inline';
+  socials.style.display = 'none';
 }
 
 function showSocials() {
-  projects.style.display = 'none'
-  about.style.display = 'none'
-  socials.style.display = 'inline'
+  projects.style.display = 'none';
+  about.style.display = 'none';
+  socials.style.display = 'inline';
 }
