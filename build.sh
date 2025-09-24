550a83fd43cbc95d94b2bcfc92856aaa5b86b21a
bomba-cat <bledion.morina@hotmail.com>
2025-09-24 20:16:46 +0200

    some update

--- build.sh
+++ build.sh
@@ -1,19 +1,10 @@
-mkdir -p build/clay                                                       \
-&& clang                                                                  \
--Wall                                                                     \
--Werror                                                                   \
--Os                                                                       \
--DCLAY_WASM                                                               \
--mbulk-memory                                                             \
---target=wasm32                                                           \
--nostdlib                                                                 \
--Wl,--strip-all                                                           \
--Wl,--export-dynamic                                                      \
--Wl,--no-entry                                                            \
--Wl,--export=__heap_base                                                  \
--Wl,--export=ACTIVE_RENDERER_INDEX                                        \
--Wl,--initial-memory=6553600                                              \
--o build/clay/index.wasm                                                  \
-main.c                                                                    \
--v																																				\
-&& cp index.html build/clay/index.html && cp -r images/ build/clay/
+#!/bin/bash
+
+mkdir -p build/clay
+clang -v -Wall -Werror -Os -DCLAY_WASM -mbulk-memory --target=wasm32 -nostdlib \
+  -Wl,--strip-all -Wl,--export-dynamic -Wl,--no-entry -Wl,--export=__heap_base \
+  -Wl,--export=ACTIVE_RENDERER_INDEX -Wl,--initial-memory=6553600 \
+  -o build/clay/index.wasm main.c
+
+cp index.html build/clay/index.html
+cp -r images/ build/clay/
